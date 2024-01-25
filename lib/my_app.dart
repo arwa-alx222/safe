a89dc383acc33e4128a057de369209a95e63e784
Arwa-Yusuf <113866723+arwa-alx222@users.noreply.github.com>
2024-01-25 13:52:28 +0200

    second commint

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:new_app/home_page.dart';
+import 'package:new_app/your_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,6 +9,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "", home: HomePage(),);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "",
+      home: HomePage(),);
   }
 }
